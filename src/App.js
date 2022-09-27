fe123c0e70b137280debdb4ae53d6b6a1850cbda
fahrulamirudin <58175401+fahrulamirudin@users.noreply.github.com>
2022-09-27 19:52:17 +0700

    TUGAS 4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
+import { Grid } from '@mui/material';
+import React, { useState } from 'react';
+
+import ContactForm from './components/ContactForm';
+import Contact from './components/Contact';
+import Header from './components/Header';
 // Uncomment untuk memuat daftar kontak
-// import contactsJSON from './data/contacts.json';
+import contactsJSON from './data/contacts.json';
 const App = () => {
   // Masukkan Header dan lakukan map untuk Contact ke dalam div App
   // untuk membuat daftar kontak bisa menggunakan MUI list
@@ -9,9 +19,25 @@ const App = () => {
 
   // Masukkan contacts yang sudah didapat dalam JSON sebagai initial state
   // Buatlah handler untuk menambahkan kontak baru yang akan dikirim ke ContactForm
+  const [contacts, setContacts] = useState(contactsJSON);
+
+  const addContact = (newContact) => {
+    setContacts([...contacts, newContact]);
+  };
 
   return (
     <div className="App">
+      <Header />
+      <Grid justifyContent="center" container spacing={2}>
+        <Grid item md={6} lg={6}>
+          <ContactForm handleClick={addContact} />
+        </Grid>
+        <Grid item md={6} lg={6}>
+          {contacts.map((contact, index) => (
+            <Contact key={index} data={contact} />
+          ))}
+        </Grid>
+      </Grid>
     </div>
   );
 };
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,12 +3,104 @@
 // dan Card
 // https://mui.com/material-ui/react-card/#basic-card
 
-const ContactForm = () => {
+import { Button, Card, CardActions, CardContent, Grid, TextField } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import { useState } from "react";
+
+const ContactForm = ({ handleClick }) => {
     // Form berisi name, phone, email, dan photo url
     // Buatlah state newContact berupa objek sesuai dengan data yang ada
+    const [newContact, setNewContact] = useState({
+        name: '',
+        phone: '',
+        email: '',
+        photo: ''
+    });
 
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setNewContact({
+            ...newContact,
+            [name]: value,
+        });
+    };
     return (
-        <> </>
+        <Card sx={{ minWidth: 275, margin: "25px" }}>
+            <CardContent>
+                <Box
+                    component="form"
+                    sx={{
+                        '& .MuiTextField-root': { m: 1, width: '90%' },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                >
+                    <div>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                id="name"
+                                label="Name"
+                                name="name"
+                                value={newContact.name}
+                                onChange={handleChange}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                id="phone"
+                                label="Phone"
+                                name="phone"
+                                value={newContact.phone}
+                                onChange={handleChange}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                id="email"
+                                label="Email"
+                                name="email"
+                                value={newContact.email}
+                                onChange={handleChange}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                id="photo"
+                                name="photo"
+                                label="Photo Url"
+                                value={newContact.photo}
+                                onChange={handleChange}
+                            />
+                        </Grid>
+                    </div>
+                </Box>
+            </CardContent>
+            <CardActions>
+                <Grid item xs={12}>
+                    <Button
+                        variant="contained"
+                        color="success"
+                        sx={{ width: '90%' }}
+                        onClick={(event) => {
+                            event.preventDefault();
+                            handleClick(newContact);
+                            setNewContact({
+                                name: '',
+                                phone: '',
+                                email: '',
+                                photo:'',
+                            });
+                        }}>
+                        Add New
+                    </Button>
+                </Grid>
+            </CardActions>
+        </Card>
     );
 }
 
